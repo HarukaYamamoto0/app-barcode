4cea0876ad57154f794937ebb1593f2067ee6c05
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2024-02-07 23:27:29 -0300

    First commit

---
+++ app/src/main/java/com/harukadev/barcode/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.harukadev.barcode
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+public class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+         super.onCreate(savedInstanceState)
+         setContentView(R.layout.activity_main)
+    }
+}
